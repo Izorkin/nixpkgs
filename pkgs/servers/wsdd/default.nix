c37d205c3b7cbdf7f8b4506826dab25e45f7f77f
Izorkin <izorkin@elven.pw>
2021-01-27 19:12:37 +0300

    wsdd: 0.6.2 -> 0.6.4

--- pkgs/servers/wsdd/default.nix
+++ pkgs/servers/wsdd/default.nix
@@ -1,30 +1,24 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper, nixosTests, python3, fetchpatch }:
+{ lib, stdenv, fetchFromGitHub, makeWrapper, nixosTests, python3 }:
 
 stdenv.mkDerivation rec {
   pname = "wsdd";
-  version = "0.6.2";
+  version = "0.6.4";
 
   src = fetchFromGitHub {
     owner = "christgau";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0444xh1r5wd0zfch1hg1f9s4cw68srrm87hqx16qvlgx6jmz5j0p";
+    sha256 = "0lfvpbk1lkri597ac4gz5x4csfyik8axz4b41i03xsqv9bci2vh6";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   buildInputs = [ python3 ];
 
-  patches = [
-    (fetchpatch {
-      # https://github.com/christgau/wsdd/issues/72
-      name = "fix_send_messages_using_correct_socket.patch";
-      url = "https://github.com/christgau/wsdd/commit/1ed74fe73a9fe2e2720859e2822116d65e4ffa5b.patch";
-      sha256 = "1n9bqvh20nhnvnc5pxvzf9kk8nky6rmbmfryg65lfmr1hmg676zg";
-    })
-  ];
-
   installPhase = ''
+    substituteInPlace src/wsdd.py \
+      --replace '2.0' '5.0'
+
     install -Dm0755 src/wsdd.py $out/bin/wsdd
     wrapProgram $out/bin/wsdd --prefix PYTHONPATH : "$PYTHONPATH"
   '';
