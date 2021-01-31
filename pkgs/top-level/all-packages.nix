53e7f1da586ab58efd538a821f09df4d2e351d92
Steven Roose <steven@stevenroose.org>
2021-01-31 23:03:23 +0100

    Add PeerTube (v3.0.1) pkg and service

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24954,6 +24954,10 @@ in
 
   peek = callPackage ../applications/video/peek { };
 
+  peertube = callPackage ../servers/peertube {
+    nodejs = pkgs.nodejs-12_x;
+  };
+
   pflask = callPackage ../os-specific/linux/pflask {};
 
   pfsshell = callPackage ../tools/misc/pfsshell { };
