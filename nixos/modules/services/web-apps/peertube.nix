3f8427dbc66dbdcc6eaaa3f83c5c542371373201
Izorkin <izorkin@elven.pw>
2021-04-05 15:03:48 +0300

    nixos/peertube: update service configuration

--- nixos/modules/services/web-apps/peertube.nix
+++ nixos/modules/services/web-apps/peertube.nix
@@ -1,25 +1,21 @@
 { lib, pkgs, config, ... }:
 
 let
-  name = "peertube";
   cfg = config.services.peertube;
 
-  uid = config.ids.uids.peertube;
-  gid = config.ids.gids.peertube;
-in
-{
+in {
   options.services.peertube = {
     enable = lib.mkEnableOption "Enable Peertube’s service";
 
     user = lib.mkOption {
       type = lib.types.str;
-      default = name;
+      default = "peertube";
       description = "User account under which Peertube runs";
     };
 
     group = lib.mkOption {
       type = lib.types.str;
-      default = name;
+      default = "peertube";
       description = "Group under which Peertube runs";
     };
 
@@ -68,7 +64,7 @@ in
 
     runtimeDir = lib.mkOption {
       type = lib.types.path;
-      default = "/var/lib/${name}";
+      default = "/var/lib/peertube";
       description = "The directory where Peertube stores its runtime data.";
     };
 
@@ -82,44 +78,10 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    users.users = lib.optionalAttrs (cfg.user == name) {
-      "${name}" = {
-        inherit uid;
-        group = cfg.group;
-        description = "Peertube user";
-        home = cfg.runtimeDir;
-        useDefaultShell = true;
-        # todo: fix this. needed for postgres authentication
-        password = "peertube";
-      };
-    };
-    users.groups = lib.optionalAttrs (cfg.group == name) {
-      "${name}" = {
-        inherit gid;
-      };
-    };
-
-    services.postgresql = lib.mkIf cfg.database.createLocally {
-      enable = true;
-      ensureUsers = [ { name = cfg.database.user; }];
-      # The database is created as a startup script of the peertube service.
-      authentication = ''
-        host ${cfg.database.name} ${cfg.database.user} 127.0.0.1/32 trust
-        host ${cfg.database.name} ${cfg.database.user} 127.0.0.1/32 md5
-      '';
-    };
-
-    services.postfix = lib.mkIf cfg.smtp.createLocally {
-      enable = true;
-    };
-
-    services.redis = lib.mkIf cfg.redis.createLocally {
-      enable = true;
-    };
-
     # Make sure the runtimeDir exists with the desired permissions.
     systemd.tmpfiles.rules = [
-      "d \"${cfg.runtimeDir}\" - ${cfg.user} ${cfg.group} - -"
+      "d '${cfg.runtimeDir}' 0750 ${cfg.user} ${cfg.group} - -"
+      "d '/var/www/peertube' 0750 ${cfg.user} ${cfg.group} - -"
     ];
 
     systemd.services.peertube = {
@@ -141,46 +103,68 @@ in
       '';
 
       serviceConfig = {
-        User = cfg.user;
-        Group = cfg.group;
-        WorkingDirectory = cfg.package;
-        StateDirectory = "peertube";
-        StateDirectoryMode = "0750";
-        PrivateTmp = true;
-        ProtectHome = true;
-        ProtectControlGroups = true;
-        ProtectSystem = "full";
-        Restart = "always";
         Type = "simple";
-        TimeoutSec = 60;
-        CapabilityBoundingSet = "~CAP_SYS_ADMIN";
         ExecStartPre = let script = pkgs.writeScript "peertube-pre-start.sh" ''
           #!/bin/sh
-          set -e
-
-          if ! [ -e "${cfg.runtimeDir}/.first_run" ]; then
-            set -v
-            if [ -e "${cfg.runtimeDir}/.first_run_partial" ]; then
-              echo "Warn: first run was interrupted"
-            fi
-            touch "${cfg.runtimeDir}/.first_run_partial"
-
-            echo "Running PeerTube's PostgreSQL initialization..."
-            echo "PeerTube is known to work with PostgreSQL v12, if any error occurs, please check your version."
-
-            sudo -u postgres "${config.services.postgresql.package}/bin/createdb" -O ${cfg.database.user} -E UTF8 -T template0 ${cfg.database.name}
-            sudo -u postgres "${config.services.postgresql.package}/bin/psql" -c "CREATE EXTENSION pg_trgm;" ${cfg.database.name}
-            sudo -u postgres "${config.services.postgresql.package}/bin/psql" -c "CREATE EXTENSION unaccent;" ${cfg.database.name}
-
-            touch "${cfg.runtimeDir}/.first_run"
-            rm "${cfg.runtimeDir}/.first_run_partial"
-          fi
+          sudo -u postgres "${config.services.postgresql.package}/bin/psql" -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;" ${cfg.database.name}
+          sudo -u postgres "${config.services.postgresql.package}/bin/psql" -c "CREATE EXTENSION IF NOT EXISTS unaccent;" ${cfg.database.name}
         '';
         in "+${script}";
+        Restart = "always";
+        RestartSec = 20;
+        TimeoutSec = 60;
+        WorkingDirectory = cfg.package;
+        # User and group
+        User = cfg.user;
+        Group = cfg.group;
+        # State directory and mode
+        StateDirectory = "peertube";
+        StateDirectoryMode = "0750";
+        # Capabilities
+        CapabilityBoundingSet = "~CAP_SYS_ADMIN";
+        # Sandboxing
+        ProtectHome = true;
+        ProtectSystem = "full";
+        PrivateTmp = true;
+        ProtectControlGroups = true;
       };
 
       unitConfig.RequiresMountsFor = cfg.runtimeDir;
     };
+
+    services.postfix = lib.mkIf cfg.smtp.createLocally {
+      enable = true;
+    };
+
+    services.redis = lib.mkIf cfg.redis.createLocally {
+      enable = true;
+    };
+
+    services.postgresql = lib.mkIf cfg.database.createLocally {
+      enable = true;
+      ensureUsers = [{
+        name = cfg.database.user;
+        ensurePermissions = { "DATABASE ${cfg.database.name}" = "ALL PRIVILEGES"; };
+      }];
+      ensureDatabases = [ cfg.database.name ];
+      authentication = ''
+        host ${cfg.database.name} ${cfg.database.user} 127.0.0.1/32 trust
+        host ${cfg.database.name} ${cfg.database.user} 127.0.0.1/32 md5
+      '';
+    };
+
+    users.users = lib.optionalAttrs (cfg.user == "peertube") {
+     peertube  = {
+        isSystemUser = true;
+        home = cfg.runtimeDir;
+        group = cfg.group;
+        # todo: fix this. needed for postgres authentication
+        password = "peertube";
+      };
+    };
+
+    users.groups = lib.optionalAttrs (cfg.group == "peertube") {
+      peertube = { };
+    };
   };
 }
-
