ede77152f8d5b9deaced334219f1604c9a6678fd
Izorkin <izorkin@elven.pw>
2021-04-06 15:47:19 +0300

    nixos/peertube: update service configuration

--- nixos/modules/services/web-apps/peertube.nix
+++ nixos/modules/services/web-apps/peertube.nix
@@ -28,19 +28,19 @@ let
       port: ${toString cfg.redis.port}
 
     storage:
-      tmp: '${cfg.runtimeDir}/storage/tmp/'
-      avatars: '${cfg.runtimeDir}/storage/avatars/'
-      videos: '${cfg.runtimeDir}/storage/videos/'
-      streaming_playlists: '${cfg.runtimeDir}/storage/streaming-playlists/'
-      redundancy: '${cfg.runtimeDir}/storage/redundancy/'
-      logs: '${cfg.runtimeDir}/storage/logs/'
-      previews: '${cfg.runtimeDir}/storage/previews/'
-      thumbnails: '${cfg.runtimeDir}/storage/thumbnails/'
-      torrents: '${cfg.runtimeDir}/storage/torrents/'
-      captions: '${cfg.runtimeDir}/storage/captions/'
-      cache: '${cfg.runtimeDir}/storage/cache/'
-      plugins: '${cfg.runtimeDir}/storage/plugins/'
-      client_overrides: '${cfg.runtimeDir}/storage/client-overrides/'
+      tmp: '/var/lib/peertube/storage/tmp/'
+      avatars: '/var/lib/peertube/storage/avatars/'
+      videos: '/var/lib/peertube/storage/videos/'
+      streaming_playlists: '/var/lib/peertube/storage/streaming-playlists/'
+      redundancy: '/var/lib/peertube/storage/redundancy/'
+      logs: '/var/lib/peertube/storage/logs/'
+      previews: '/var/lib/peertube/storage/previews/'
+      thumbnails: '/var/lib/peertube/storage/thumbnails/'
+      torrents: '/var/lib/peertube/storage/torrents/'
+      captions: '/var/lib/peertube/storage/captions/'
+      cache: '/var/lib/peertube/storage/cache/'
+      plugins: '/var/lib/peertube/storage/plugins/'
+      client_overrides: '/var/lib/peertube/storage/client-overrides/'
 
     ${cfg.extraConfig}
   '';
@@ -157,12 +157,6 @@ in {
       };
     };
 
-    runtimeDir = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/lib/peertube";
-      description = "The directory where Peertube stores its runtime data.";
-    };
-
     package = lib.mkOption {
       type = lib.types.package;
       default = pkgs.peertube;
@@ -173,11 +167,11 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    # Make sure the runtimeDir exists with the desired permissions.
     systemd.tmpfiles.rules = [
-      "d '${cfg.runtimeDir}' 0750 ${cfg.user} ${cfg.group} - -"
-      "d '${cfg.runtimeDir}/config' 0700 ${cfg.user} ${cfg.group} - -"
-      "d '${cfg.runtimeDir}/storage' 0750 ${cfg.user} ${cfg.group} - -"
+      "d '/var/lib/peertube/config' 0700 ${cfg.user} ${cfg.group} - -"
+      "z '/var/lib/peertube/config' 0700 ${cfg.user} ${cfg.group} - -"
+      "d '/var/lib/peertube/storage' 0750 ${cfg.user} ${cfg.group} - -"
+      "z '/var/lib/peertube/storage' 0750 ${cfg.user} ${cfg.group} - -"
     ];
 
     systemd.services.peertube = {
@@ -188,8 +182,9 @@ in {
       wants = [ "redis.service" ]
         ++ (if databaseActuallyCreateLocally then [ "postgresql.service" ] else []);
 
-      environment.NODE_CONFIG_DIR = "${cfg.runtimeDir}/config";
+      environment.NODE_CONFIG_DIR = "/var/lib/peertube/config";
       environment.NODE_ENV = "production";
+      environment.NODE_EXTRA_CA_CERTS = "/etc/ssl/certs/ca-certificates.crt";
       environment.HOME = cfg.package;
 
       path = [ pkgs.nodejs pkgs.bashInteractive pkgs.ffmpeg pkgs.openssl pkgs.sudo pkgs.youtube-dl ];
@@ -198,7 +193,8 @@ in {
         Type = "simple";
         ExecStartPre = let preStartScript = pkgs.writeScript "peertube-pre-start.sh" ''
           #!/bin/sh
-          cat > ${cfg.runtimeDir}/config/local-production.yaml <<EOF
+          umask 077
+          cat > /var/lib/peertube/config/local-production.yaml <<EOF
           ${lib.optionalString ((!databaseActuallyCreateLocally) && (cfg.database.passwordFile != null)) ''
           database:
             password: '$(cat ${cfg.database.passwordFile})'
@@ -217,17 +213,17 @@ in {
             auth: '$(cat ${cfg.redis.passwordFile})'
           ''}
           EOF
+          chown ${cfg.user}:${cfg.group} /var/lib/peertube/config/local-production.yaml
           ${lib.optionalString databaseActuallyCreateLocally ''
           sudo -u postgres ${config.services.postgresql.package}/bin/psql -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;" ${cfg.database.name}
           sudo -u postgres ${config.services.postgresql.package}/bin/psql -c "CREATE EXTENSION IF NOT EXISTS unaccent;" ${cfg.database.name}
           ''}
+          sudo -u ${cfg.user} ln -sf ${cfg.package}/config/default.yaml /var/lib/peertube/config/default.yaml
+          sudo -u ${cfg.user} ln -sf ${configFile} /var/lib/peertube/config/production.yaml
         '';
         in "+${preStartScript}";
         ExecStart = let startScript = pkgs.writeScript "peertube-start.sh" ''
           #!/bin/sh
-          install -m 0750 -d ${cfg.runtimeDir}/config
-          ln -sf ${cfg.package}/config/default.yaml ${cfg.runtimeDir}/config/default.yaml
-          ln -sf ${configFile} ${cfg.runtimeDir}/config/production.yaml
           exec npm start
         '';
         in "${startScript}";
@@ -241,6 +237,8 @@ in {
         # State directory and mode
         StateDirectory = "peertube";
         StateDirectoryMode = "0750";
+        # Access write directories
+        UMask = "0027";
         # Capabilities
         CapabilityBoundingSet = "~CAP_SYS_ADMIN";
         # Sandboxing
@@ -249,8 +247,6 @@ in {
         PrivateTmp = true;
         ProtectControlGroups = true;
       };
-
-      unitConfig.RequiresMountsFor = cfg.runtimeDir;
     };
 
     services.postfix = lib.mkIf cfg.smtp.createLocally {
@@ -280,7 +276,6 @@ in {
       (lib.mkIf (cfg.user == "peertube") {
         peertube = {
           isSystemUser = true;
-          home = cfg.runtimeDir;
           group = cfg.group;
         };
       })
